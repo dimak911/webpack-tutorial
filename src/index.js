4e06f500683f39335477ec75897fc136a5da7e1b
Dmytro Kniahnytskyi <dimak911@gmail.com>
2022-05-24 21:52:15 +0300

    webpack tutor setup

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import example from "./images/example.jpg";
+import worker from "./images/worker.svg";
+import "./styles/main.scss";
+// Create a class property without a constructor
+class Game {
+  name = "Violin Charades";
+}
+const myGame = new Game();
+// Create paragraph node
+const p = document.createElement("p");
+p.textContent = `I like ${myGame.name}.`;
+
+console.log("Interesting!");
+
+// Create heading node
+const heading = document.createElement("h1");
+heading.textContent = "Interesting!";
+
+// Append heading node to the DOM
+const app = document.querySelector("#root");
+app.append(heading);
